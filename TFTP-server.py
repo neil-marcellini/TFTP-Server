c94bb24cc341c90c207e0a723f4ffa6b75c4ff47
neil-marcellini <neil.marcellini@gmail.com>
2021-02-11 19:58:15 -0800

    getting opcode again

--- TFTP-server.py
+++ TFTP-server.py
@@ -1,12 +1,11 @@
 import socket
 HOST = ''
-PORT = 69
+PORT = 6969
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     print("TFTP Server Listening")
     while True:
-        data, addr = sock.recvfrom(1024)
-        byte_list = bytearray(data)
-        opcode = int.from_bytes(byte_list[:2], byteorder='big')
+        data, addr = s.recvfrom(1024)
+        opcode = int.from_bytes(data[:2], byteorder='big')
         print(f"opcode = {opcode}")
