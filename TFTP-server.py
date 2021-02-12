3ad1a5b74bcd62224df6456755af53e39680e6e8
neil-marcellini <neil.marcellini@gmail.com>
2021-02-11 17:26:10 -0800

    attempt to get opcode

---
+++ TFTP-server.py
@@ -0,0 +1,12 @@
+import socket
+HOST = ''
+PORT = 69
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST, PORT))
+    print("TFTP Server Listening")
+    while True:
+        data, addr = sock.recvfrom(1024)
+        byte_list = bytearray(data)
+        opcode = int.from_bytes(byte_list[0:3], byteorder='big')
+        print(f"opcode = {opcode}")
