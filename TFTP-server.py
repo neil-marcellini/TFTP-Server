aeed135fa6a4735a326d1d921b450a1719943c3a
neil-marcellini <neil.marcellini@gmail.com>
2021-02-12 16:36:53 -0800

    got file_name

--- TFTP-server.py
+++ TFTP-server.py
@@ -5,7 +5,21 @@ PORT = 6969
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     print("TFTP Server Listening")
+    file_name_start = 2
     while True:
         data, addr = s.recvfrom(1024)
-        opcode = int.from_bytes(data[:2], byteorder='big')
+        byte_data = bytearray(data)
+        print(byte_data)
+        opcode = int.from_bytes(data[:file_name_start], byteorder='big')
         print(f"opcode = {opcode}")
+        if opcode != 1: print("Not a RRQ, closing")
+        # get file name
+        byte_data = byte_data[file_name_start:]
+        null_byte = b'\x00'
+        print(f"null_byte = {null_byte}")
+        zero_index = byte_data.index(null_byte) + file_name_start
+        print(zero_index)
+        file_name = data[file_name_start:zero_index]
+        file_name = file_name.decode('utf-8')
+        print(f"file_name = {file_name}")
+            
